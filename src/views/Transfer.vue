235006bedfc6f095bb150cc2bb18a434a3561c23
Waru Clan <101069332+WaruClan@users.noreply.github.com>
2022-12-01 22:50:29 +0000

    Update Transfer.vue

--- src/views/Transfer.vue
+++ src/views/Transfer.vue
@@ -5,9 +5,15 @@
 
 
 <template>
-    <div class=""WaruXswap">
+    <div class="flex flex-1 flex-col justify-between items-center lg:px-20 max-w-screen-xl mx-auto">
         <iframe
-            src="https://app.debridge.finance/assets/scripts/widget.js'
-            deBridge.widget({"v":"1","element":"WaruXswap","title":"WaruXswap","width":"600","height":"800","r":4631,"affiliateFeePercent":"0.3","affiliateFeeRecipient":"0x9fdcf9e4c3b9c8606803fada2734fabda2d24dc8","supportedChains":"{\"inputChains\":{\"1\":\"all\",\"56\":\"all\",\"137\":\"all\",\"42161\":\"all\",\"43114\":\"all\"},\"outputChains\":{\"1\":\"all\",\"56\":\"all\",\"137\":\"all\",\"42161\":\"all\",\"43114\":\"all\"}}","inputChain":137,"outputChain":137,"inputCurrency":"","outputCurrency":"0xe3627374ac4baf5375e79251b0af23afc450fc0e","address":"","showSwapTransfer":true,"amount":"25","lang":"en","mode":"deswap","isEnableBundle":false,"styles":"eyJiYWRnZSI6IiM5MzkzOTMiLCJib3JkZXJSYWRpdXMiOjgsInByaW1hcnkiOiJyZ2JhKDU2LDI1NSwxMDksMC43MSkiLCJmb250RmFtaWx5IjoiIn0=","theme":"dark"})>
+            src='https://widget.xp.network/?widget=true&background=000000&panelBackground=bdbdbd&modalBackground=a6a6a6&color=000000&fontSize=16&btnColor=22e288&btnBackground=000000&btnRadius=10&fontFamily=Open Sans&chains=Polygon-Harmony&from=Harmony&to=Polygon&cardBackground=51649a&cardBackgroundBot=1e222d&cardColor=ffffff&cardRadius=25&secondaryColor=0c0d0d&accentColor=0529a8&borderColor=988b8b&iconColor=22e288&tooltipBg=1D212A&tooltipColor=ffffff&wallets=MetaMask-WalletConnect&bridgeState=undefined&showLink=true&affiliationFees=1.05'
+            frameborder='0' width="100%" height="800px"></iframe>
     </div>
 </template>
+
+<div id="WaruXswap"></div>
+<script src="https://app.debridge.finance/assets/scripts/widget.js"></script>
+<script>
+deBridge.widget({"v":"1","element":"WaruXswap","title":"WaruXswap","width":"600","height":"800","r":4631,"affiliateFeePercent":"0.3","affiliateFeeRecipient":"0x9fdcf9e4c3b9c8606803fada2734fabda2d24dc8","supportedChains":"{\"inputChains\":{\"1\":\"all\",\"56\":\"all\",\"137\":\"all\",\"42161\":\"all\",\"43114\":\"all\"},\"outputChains\":{\"1\":\"all\",\"56\":\"all\",\"137\":\"all\",\"42161\":\"all\",\"43114\":\"all\"}}","inputChain":137,"outputChain":137,"inputCurrency":"","outputCurrency":"0xe3627374ac4baf5375e79251b0af23afc450fc0e","address":"","showSwapTransfer":true,"amount":"25","lang":"en","mode":"deswap","isEnableBundle":false,"styles":"eyJiYWRnZSI6IiM5MzkzOTMiLCJib3JkZXJSYWRpdXMiOjgsInByaW1hcnkiOiJyZ2JhKDU2LDI1NSwxMDksMC43MSkiLCJmb250RmFtaWx5IjoiIn0=","theme":"dark"}) 
+</script>
