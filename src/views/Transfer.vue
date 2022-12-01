bc6cf0251f8ad83666972c48c56ec17b398b6013
Waru Clan <101069332+WaruClan@users.noreply.github.com>
2022-12-01 23:05:32 +0000

    Update Transfer.vue

--- src/views/Transfer.vue
+++ src/views/Transfer.vue
@@ -7,7 +7,7 @@
 <template>
     <div class="flex flex-1 flex-col justify-between items-center lg:px-20 max-w-screen-xl mx-auto">
         <iframe
-            src='https://app.debridge.finance/assets/scripts/widget.js'
+            src='https://app.debridge.finance/deswap?inputChain=137&inputCurrency=&outputChain=137&outputCurrency=0xE3627374Ac4baf5375e79251B0af23aFc450FC0E&r=4631&address=0x0000000000000000000000000000000000000000&amount=25'
             frameborder='0' width="100%" height="800px"></iframe>
     </div>
 </template>
