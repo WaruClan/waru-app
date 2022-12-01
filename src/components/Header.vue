fe67d1648416f3305e62c66bdfab604f8334a1f1
Waru Clan <101069332+WaruClan@users.noreply.github.com>
2022-12-01 21:52:20 +0000

    test deswap

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -38,4 +38,10 @@ const routes = [
             </router-link>
         </div>
     </header>
+<div id="WaruXswap"></div>
+<script src="https://app.debridge.finance/assets/scripts/widget.js"></script>
+<script>
+deBridge.widget({"v":"1","element":"WaruXswap","title":"WaruXswap","width":"600","height":"800","r":4631,"affiliateFeePercent":"0.3","affiliateFeeRecipient":"0x9fdcf9e4c3b9c8606803fada2734fabda2d24dc8","supportedChains":"{\"inputChains\":{\"1\":\"all\",\"56\":\"all\",\"137\":\"all\",\"42161\":\"all\",\"43114\":\"all\"},\"outputChains\":{\"1\":\"all\",\"56\":\"all\",\"137\":\"all\",\"42161\":\"all\",\"43114\":\"all\"}}","inputChain":137,"outputChain":137,"inputCurrency":"","outputCurrency":"0xe3627374ac4baf5375e79251b0af23afc450fc0e","address":"","showSwapTransfer":true,"amount":"25","lang":"en","mode":"deswap","isEnableBundle":false,"styles":"eyJiYWRnZSI6IiM5MzkzOTMiLCJib3JkZXJSYWRpdXMiOjgsInByaW1hcnkiOiJyZ2JhKDU2LDI1NSwxMDksMC43MSkiLCJmb250RmFtaWx5IjoiIn0=","theme":"dark"}) 
+</script>
+
 </template>
