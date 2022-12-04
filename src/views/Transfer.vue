4b7dc82ea0c5c3af74e41503c1d75a65620058cb
Waru Clan <101069332+WaruClan@users.noreply.github.com>
2022-12-04 18:36:43 +0000

    Update Transfer.vue

--- src/views/Transfer.vue
+++ src/views/Transfer.vue
@@ -4,10 +4,8 @@
 
 
 
-<template>
-    <iframe
-  src="https://app.debridge.finance/deswap?inputChain=137&inputCurrency=&outputChain=137&outputCurrency=0xE3627374Ac4baf5375e79251B0af23aFc450FC0E&r=4631&address=0x0000000000000000000000000000000000000000&amount=25"
-  frameborder="0"
-  width="100%"
-  height="800px"
-></iframe>
+<div id="WaruXswap"></div>
+<script src="https://app.debridge.finance/assets/scripts/widget.js%22%3E</script>
+<script>
+deBridge.widget({"v":"1","element":"WaruXswap","title":"WaruXswap","width":"600","height":"800","r":4631,"affiliateFeePercent":"0.3","affiliateFeeRecipient":"0x9fdcf9e4c3b9c8606803fada2734fabda2d24dc8","supportedChains":"{"inputChains":{"1":"all","56":"all","137":"all","42161":"all","43114":"all"},"outputChains":{"1":"all","56":"all","137":"all","42161":"all","43114":"all"}}","inputChain":137,"outputChain":137,"inputCurrency":"","outputCurrency":"0xe3627374ac4baf5375e79251b0af23afc450fc0e","address":"","showSwapTransfer":true,"amount":"25","lang":"en","mode":"deswap","isEnableBundle":false,"styles":"eyJiYWRnZSI6IiM5MzkzOTMiLCJib3JkZXJSYWRpdXMiOjgsInByaW1hcnkiOiJyZ2JhKDU2LDI1NSwxMDksMC43MSkiLCJmb250RmFtaWx5IjoiIn0=","theme":"dark"}) 
+</script>
