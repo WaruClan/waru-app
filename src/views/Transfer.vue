1571e22f74792b5327c46ccba2db30535f16fa9a
Waru Clan <101069332+WaruClan@users.noreply.github.com>
2023-01-07 12:49:57 +0000

    Update Transfer.vue

--- src/views/Transfer.vue
+++ src/views/Transfer.vue
@@ -1,11 +1,2 @@
-<script type="text/javascript">
-
-</script>
-
-
-
-<div id="deWaruX"></div>
-<script src="https://app.debridge.finance/assets/scripts/widget.js"></script>
-<script>
-deBridge.widget({"v":"1","element":"deWaruX","title":"deWaruX","width":"600","height":"800","r":4631,"supportedChains":"{\"inputChains\":{\"137\":\"all\"},\"outputChains\":{\"1\":\"all\",\"56\":\"all\",\"137\":\"all\",\"42161\":\"all\",\"43114\":\"all\"}}","inputChain":137,"outputChain":137,"inputCurrency":"","outputCurrency":"0xe3627374ac4baf5375e79251b0af23afc450fc0e","address":"","showSwapTransfer":false,"amount":"10","lang":"en","mode":"deswap","isEnableBundle":false,"styles":"eyJib3JkZXJSYWRpdXMiOjgsImZvbnRGYW1pbHkiOiIifQ==","theme":"dark"}) 
-</script>
+<iframe src="https://www.sushi.com/earn/matic:0x96f0ececb9d5b2cc0238712a6a987a5102d3b0f7"
+width="600" height="800"></iframe>
