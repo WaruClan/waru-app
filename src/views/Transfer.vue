c8ed6e4991e7b9f9f9cd743d539b5c98113cc796
Waru Clan <101069332+WaruClan@users.noreply.github.com>
2023-01-08 23:02:09 +0000

    Update Transfer.vue

--- src/views/Transfer.vue
+++ src/views/Transfer.vue
@@ -1,2 +1,4 @@
-<iframe src="https://www.sushi.com/earn/matic:0x96f0ececb9d5b2cc0238712a6a987a5102d3b0f7"
-width="600" height="800"></iframe>
+<script>
+  document.domain = "openocean.net";
+</script>
+<iframe src="https://widget.openocean.finance/CLASSIC#/POLYGON/MATIC/WARU"></iframe>
