ecfc608f8ed620aa29cc86827c0ac204c8b25374
Waru Clan <101069332+WaruClan@users.noreply.github.com>
2023-01-08 23:32:56 +0000

    Update Transfer.vue

--- src/views/Transfer.vue
+++ src/views/Transfer.vue
@@ -1,9 +1,12 @@
-  <div>
-    <iframe src="https://widget.openocean.finance/CLASSIC#/POLYGON/MATIC/WARU" frameborder="0" style="width: 100%; height: 100%;"></iframe>
-  </div>
-
-<script>
-export default {
-  name: 'OpenOceanWidget',
-};
+<script setup lang="ts">
 </script>
+
+
+
+<template>
+    <div class="flex flex-1 flex-col justify-between items-center lg:px-20 max-w-screen-xl mx-auto">
+        <iframe
+            src='https://widget.openocean.finance/CLASSIC#/POLYGON/MATIC/WARU'
+            frameborder='0' width="100%" height="800px"></iframe>
+    </div>
+</template>
