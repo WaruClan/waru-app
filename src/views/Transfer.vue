caa97ee77f17ce57b65358fee3bdaf3adffbfdcd
Waru Clan <101069332+WaruClan@users.noreply.github.com>
2023-07-02 21:52:48 +0100

    Update Transfer.vue

--- src/views/Transfer.vue
+++ src/views/Transfer.vue
@@ -6,7 +6,7 @@
 <template>
     <div class="flex flex-1 flex-col justify-between items-center lg:px-20 max-w-screen-xl mx-auto">
         <iframe
-            src='https://www.sushi.com/earn/matic:0x96f0ececb9d5b2cc0238712a6a987a5102d3b0f7'
+            srchttps://www.sushi.com/pool/137:0x96f0ececb9d5b2cc0238712a6a987a5102d3b0f7'
             frameborder='0' width="100%" height="800px"></iframe>
     </div>
 </template>
