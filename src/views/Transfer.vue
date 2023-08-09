74b8e84e71e118aa786a8f436e7fb6abba4061c8
Waru Clan <101069332+WaruClan@users.noreply.github.com>
2023-08-09 01:01:49 +0100

    Update Transfer.vue

--- src/views/Transfer.vue
+++ src/views/Transfer.vue
@@ -4,7 +4,7 @@
 
 
 <template>
-    <div>
+    <div  class="flex flex-1 flex-col justify-between items-center lg:px-20 max-w-screen-xl mx-auto">
         <iframe
             src="https://waruclan.xyz/"
             frameborder='0' width="100%" height="800px"></iframe>
